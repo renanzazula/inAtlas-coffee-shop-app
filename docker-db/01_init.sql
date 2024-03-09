fead7e5cba9a5f79f3e49df76c4544222765071e
renanzazula <renanzazula@gmail.com>
2024-03-09 15:23:46 +0100

    delete no in user fix init file db

--- docker-db/01_init.sql
+++ docker-db/01_init.sql
@@ -155,7 +155,45 @@ CREATE TABLE IF NOT EXISTS `standardcoffeeshopapp`.`user` (
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
 
-
+-- -----------------------------------------------------
+-- Table `standardcoffeeshopapp`.`user_pass`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `standardcoffeeshopapp`.`user_pass`
+(
+    `id`
+    INT
+    NOT
+    NULL,
+    `password`
+    VARCHAR
+(
+    255
+) NULL,
+    `user_id` BIGINT NOT NULL,
+    PRIMARY KEY
+(
+    `id`
+),
+    INDEX `fk_user_pass_user1_idx`
+(
+    `user_id`
+    ASC
+) VISIBLE,
+    CONSTRAINT `fk_user_pass_user1`
+    FOREIGN KEY
+(
+    `user_id`
+)
+    REFERENCES `standardcoffeeshopapp`.`user`
+(
+    `id`
+)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+    ) ENGINE = InnoDB
+    AUTO_INCREMENT = 4
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
 -- -----------------------------------------------------
 -- Table `standardcoffeeshopapp`.`login_failure`
 -- -----------------------------------------------------
@@ -360,6 +398,14 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 INSERT INTO `standardcoffeeshopapp`.`user`(`id`,`username`,`password`,`account_non_expired`,`account_non_locked`,`credential_non_expired`,`enable`, `use_google_2fa`) VALUES(2, 'user',     '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', true, true, true, true, false);
 INSERT INTO `standardcoffeeshopapp`.`user`(`id`,`username`,`password`,`account_non_expired`,`account_non_locked`,`credential_non_expired`,`enable`, `use_google_2fa`) VALUES(3, 'customer', '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', true, true, true, true, false);
 INSERT INTO `standardcoffeeshopapp`.`user`(`id`,`username`,`password`,`account_non_expired`,`account_non_locked`,`credential_non_expired`,`enable`, `use_google_2fa`) VALUES(1, 'admin',    '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', true, true, true, true, false);
+
+INSERT INTO `standardcoffeeshopapp`.`user_pass`(`id`, `password``user_id`)
+VALUES (2, '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', 2);
+INSERT INTO `standardcoffeeshopapp`.`user_pass`(`id`, `password``user_id`)
+VALUES (3, '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', 3);
+INSERT INTO `standardcoffeeshopapp`.`user_pass`(`id`, `password``user_id`)
+VALUES (1, '{bcrypt}$2a$10$3w//CgAicchkVo0iCYJ0W.oXv911VdgfHsuS0hFks5joDpFFakUS2', 1);
+
 INSERT INTO `standardcoffeeshopapp`.`role` (`id`,`name`) VALUES(1, 'ADMIN');
 INSERT INTO `standardcoffeeshopapp`.`role` (`id`,`name`) VALUES(2, 'USER');     -- user application internal
 INSERT INTO `standardcoffeeshopapp`.`role` (`id`,`name`) VALUES(3, 'CUSTOMER'); -- user application external or second role
@@ -395,26 +441,97 @@ INSERT INTO `standardcoffeeshopapp`.`role_authority` (`authority_id`, `role_id`)
 INSERT INTO `standardcoffeeshopapp`.`user_role` (`role_id`, `user_id`) VALUES(1, 1);
 INSERT INTO `standardcoffeeshopapp`.`user_role` (`role_id`, `user_id`) VALUES(2, 2);
 INSERT INTO `standardcoffeeshopapp`.`user_role` (`role_id`, `user_id`) VALUES(3, 3);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (1, 'Latte',      'Latte',      'available', 100,   5.3, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (2, 'Espresso',   'Espresso',   'available', 100,   4.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (3, 'Sandwich',   'Sandwich',   'available', 100, 10.10, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (4, 'Milk',       'Milk',       'available', 100,   1.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (5, 'Cake Slice', 'Cake Slice', 'available', 100,   9.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (6, 'Capuccino',  'Capuccino',  'available', 100,   8.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,`who_update`, `who_create`, `date_update`, `date_create`, `time_create`, `version`) VALUES (7, 'Tea',        'Tea',        'available', 100,   6.1, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0);
-INSERT INTO `standardcoffeeshopapp`.`discount` (`id`, `date_create`, `date_update`, `time_create`, `time_update`, `version`, `who_create`, `who_update`, `discount`, `start_date`, `status`, `to_date`, quantity_items, discount_type) VALUES (1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),CURRENT_TIMESTAMP(), 1, 0, 0, 5.0, CURRENT_TIMESTAMP(), 'Active',CURRENT_TIMESTAMP(), 8, 'DISCOUNT_ORDER');
-INSERT INTO `standardcoffeeshopapp`.`order_request` (`id`, `date_create`, `date_update`, `time_create`, `time_update`, `version`, `who_create`, `who_update`, `order_date_time`, `promotion_discount`, `status`, `total_amount`, `discount`, `total_quantity`) VALUES (1, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 0, CURRENT_TIMESTAMP(), 0,'CLOSE', 21.1, 0.0, 4);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (1, 0, 1, 6.1, 7);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (2, 0, 1, 4.0, 2);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (3, 0, 1, 4.0, 2);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (4, 0, 1, 6.0, 6);
-INSERT INTO `standardcoffeeshopapp`.`order_request` (`id`, `date_create`, `date_update`, `time_create`, `time_update`, `version`, `who_create`, `who_update`, `order_date_time`, `promotion_discount`, `status`, `total_amount`, `discount`, `total_quantity`) VALUES (2, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 0, CURRENT_TIMESTAMP(), 0,'OPEN', 12.0, 0.0, 4);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (6 , 0, 2,   4.0, 2);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (5 , 0, 2,   5.3, 1);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (7 , 0, 2, 10.10, 3);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (8 , 0, 2,   1.0, 4);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (9 , 0, 2,   9.0, 5);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (10, 0, 2,   8.0, 6);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (11, 0, 2,   8.0, 6);
-INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`, `order_request_id`, `price_unit`, `product_id`) VALUES (12, 0, 2,   8.0, 6);
+
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('1', 'Latte', 'Latte', 'available', 100, 5.3, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        CURRENT_TIMESTAMP(), 0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('2', 'Espresso', 'Espresso', 'available', 100, 4.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        CURRENT_TIMESTAMP(), 0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('3', 'Sandwich', 'Sandwich', 'available', 100, 10.10, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        CURRENT_TIMESTAMP(), 0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('4', 'Milk', 'Milk', 'available', 100, 1.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('5', 'Cake Slice', 'Cake Slice', 'available', 100, 9.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        CURRENT_TIMESTAMP(), 0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('6', 'Capuccino', 'Capuccino', 'available', 100, 8.0, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        CURRENT_TIMESTAMP(), 0);
+INSERT INTO `standardcoffeeshopapp`.`product` (`id`, `name`, `description`, `status`, `quantity`, `price_unit`,
+                                               `who_update`, `who_create`, `date_update`, `date_create`, `time_create`,
+                                               `version`)
+VALUES ('7', 'Tea', 'Tea', 'available', 100, 6.1, 0, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(),
+        0);
+
+INSERT INTO `standardcoffeeshopapp`.`discount` (`id`, `date_create`, `date_update`, `time_create`, `time_update`,
+                                                `version`, `who_create`, `who_update`, `discount`, `start_date`,
+                                                `status`, `to_date`, quantity_items, discount_type)
+VALUES ('1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 1, 0, 0, 5.0,
+        CURRENT_TIMESTAMP(), 'Active', CURRENT_TIMESTAMP(), 8, 'DISCOUNT_ORDER');
+
+INSERT INTO `standardcoffeeshopapp`.`order_request` (`id`, `date_create`, `date_update`, `time_create`, `time_update`,
+                                                     `version`, `who_create`, `who_update`, `order_date_time`,
+                                                     `promotion_discount`, `status`, `total_amount`, `discount`,
+                                                     `total_quantity`)
+VALUES ('1', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 0,
+        CURRENT_TIMESTAMP(), 0, 'CLOSE', 21.1, 0.0, 4);
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('1', 0, '1', 6.1, '7');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('2', 0, '1', 4.0, '2');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('3', 0, '1', 4.0, '2');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('4', 0, '1', 6.0, '6');
+
+INSERT INTO `standardcoffeeshopapp`.`order_request` (`id`, `date_create`, `date_update`, `time_create`, `time_update`,
+                                                     `version`, `who_create`, `who_update`, `order_date_time`,
+                                                     `promotion_discount`, `status`, `total_amount`, `discount`,
+                                                     `total_quantity`)
+VALUES ('2', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP(), 0, 0, 0,
+        CURRENT_TIMESTAMP(), 0, 'OPEN', 12.0, 0.0, 4);
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('6', 0, '2', 4.0, '2');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('5', 0, '2', 5.3, '1');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('7', 0, '2', 10.10, '3');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('8', 0, '2', 1.0, '4');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('9', 0, '2', 9.0, '5');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('10', 0, '2', 8.0, '6');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('11', 0, '2', 8.0, '6');
+INSERT INTO `standardcoffeeshopapp`.`order_request_request_item` (`order_request_items_id`, `discount`,
+                                                                  `order_request_id`, `price_unit`, `product_id`)
+VALUES ('12', 0, '2', 8.0, '6');
+
 COMMIT;
